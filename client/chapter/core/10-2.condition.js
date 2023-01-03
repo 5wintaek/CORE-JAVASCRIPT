ee623e10bbbb99097750be693873215dd3143d58
5wintaek <elqkddl@naver.com>
2023-01-03 14:20:21 +0900

    [function] 유사배열 및 instance 와 static 차이

---
+++ client/chapter/core/10-2.condition.js
@@ -0,0 +1,69 @@
+/* ---------------------------------------------------------------------- */
+/* Functions → Expression                                                 */
+/* ---------------------------------------------------------------------- */
+
+// ! 선언문
+function calcTotal(moneyA, moneyB, moneyC, moneyD) {
+  return moneyA + moneyB + moneyC + moneyD;
+}
+
+let resultX = calcTotal(10000, 8900, 1360, 2100);
+let resultY = calcTotal(21500, 3200, 9800, 4700);
+let resultZ = calcTotal(9000, -2500, 5000, 11900);
+
+// console.log(resultX);
+// console.log(resultY);
+// console.log(resultZ);
+
+// 함수 선언 → 일반 함수 (표현)식
+// console.log(arguments);
+// 함수 안에서만 접근 가능한 인수들의 집합 객채로서 배열과 유사하여 유사 배열 타입으로 불리는 변수 : arguments
+// arguments 객체를 사용해 함수의 매개변수 없이 아이템의 총합을 구할 수 console.error('있다',있다)
+// 유사배열은 배열이 아니다. 고로 배열의 능력을 사용하려면 진.짜 배열로 만들어야한다
+// 배열의 능력 : forEach, reduce
+let calculateTotal = function (moneyA, moneyB, moneyC, moneyD) {
+  // console.log(arguments); // 함수 안에서 사용할수있는 유사배열
+
+  let total = 0;
+
+  // ? argument 자체를 배열로 만드는법
+
+  let arr = Array.from(arguments); // static method -> 함수 자체에서 제공해주는 것 / 배열 아님 유사배열임
+  // let arr = Array.prototype.slice.call(arguments);
+
+  /*  arr.forEach(function (item, index) {
+    // foreach 안에 들어가 있는 함수가 콜백함수
+    total += item;
+  }); */
+
+  return arr.reduce(function (acc, item) {
+    return acc + item;
+  });
+
+  /* for (let value of arguments) {
+    total += value;
+  } */
+
+  /* for (let i = 0; i < arguments.length; i++) {
+    total += arguments[i];
+  } */
+  // return moneyA + moneyB + moneyC + moneyD;
+
+  return total;
+};
+
+console.log(calculateTotal(100, 500, 300, 150, 400, 150)); // 값이 더 늘어나면 무시함
+// 익명(이름이 없는) 함수 (표현)식
+let anonymousFunctionExpression = function () {};
+
+// 유명(이름을 가진) 함수 (표현)식
+let namedFunctionExpression = function hello() {};
+
+// 콜백 함수 (표현)식
+let callbackFunctionExpression;
+
+// 함수 선언문 vs. 함수 (표현)식
+
+// 즉시 실행 함수 (표현)식
+// Immediately Invoked Function Expression
+let IIFE;
