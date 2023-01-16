a0bc7f84f5d559e58ab88812117403ec1365cf83
5wintaek <elqkddl@naver.com>
2023-01-16 11:14:30 +0900

    [2023-01-16] IEFF 패턴 전

--- README.md
+++ README.md
@@ -7,3 +7,10 @@
 해당 학습 자료는 해당 브랜치로 이동 후 확인 가능합니다.
 
 [JavaScript Core 브랜치](https://www.naver.com)
+
+
+## [ 주사위 굴리기 ]
+1. dice 애니메이션 불러오기
+2. bindEvent 유틸함수 만들기
+3. handlerRollingDice 함수 만들고 토글로 애니메이션 제어하기
+4. 변수 보호를 위한 클로저 + IIFE 사용하기
--- client/main.js
+++ client/main.js
@@ -1,2 +1,34 @@
 
-import { diceAnimation  } from "./lib/index.js";
+import { diceAnimation, getNode  } from "./lib/index.js";
+
+
+
+
+
+
+// 주사위 굴리기 노드 잡기
+const rollingDiceButton = getNode('.buttonGroup > button:nth-child(1)')
+
+
+
+const handlerRollingDice = () => {
+
+  let isRolling = false
+  let stopAnimation;
+  
+  return () =>{
+    if(!isRolling){
+      stopAnimation = setInterval(diceAnimation,100) 
+    }else{
+      clearInterval(stopAnimation)
+    }
+    isRolling = !isRolling
+  }
+}
+
+
+
+
+
+
+rollingDiceButton.addEventListener('click',handlerRollingDice)
