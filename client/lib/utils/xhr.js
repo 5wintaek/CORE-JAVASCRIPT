9c34235dd296789f49ae717f287ea32f02017797
5wintaek <elqkddl@naver.com>
2023-01-17 14:02:53 +0900

    forEach 및 object.entries

--- client/lib/utils/xhr.js
+++ client/lib/utils/xhr.js
@@ -7,19 +7,32 @@
 */
 
 // xhrData 함수 만들기
-function xhrData(method,url,body){
-  const xhr = new XMLHttpRequest()
+function xhrData({
+  url = '',
+  method = 'GET',
+  body = null,
+  headers = {
+    'Content-Type':'application/json'
+   }
+}){
 
+// const{method,url,body} =  options
 
+const xhr = new XMLHttpRequest()
 // 비동기 통신 오픈 시작을 알림
 xhr.open(method,url)
 
-// 변경이 일어날떄마다 호출하는 역할
+Object.entries(headers).forEach(([key,value])=>{
+  xhr.setRequestHeader(key,value)
+})
+
 xhr.addEventListener("readystatechange", () => {
-  if(xhr.status >= 200 && xhr.status < 400){
-    if(xhr.readyState === 4){
+  const {status,readyState,response} = xhr; // 객체 구조 분해 할당 
+  
+  if(status >= 200 && xhr.status < 400){
+    if(readyState === 4){
       console.log('통신성공');
-      console.log(JSON.parse(xhr.response));
+      console.log(JSON.parse(response));
     }
   }else{
     console.log('통신 실패');
@@ -30,28 +43,33 @@ xhr.addEventListener("readystatechange", () => {
 xhr.send(JSON.stringify(body))
 }
 
+xhrData({
+  url: 'https://jsonplaceholder.typicode.com/users',
+
+})
+
 // 어떠한 행동, 여기 사이트에서 , 이걸로 통신해줘
 // 아 기존을 갖고있던게 10 개인데 우리가 POST로 1개 더 만들어서 11번
-xhrData('POST','https://jsonplaceholder.typicode.com/users',{
-  "id": 1,
-  "name": "Leanne Graham",
-  "username": "Bret",
-  "email": "Sincere@april.biz",
-  "address": {
-    "street": "Kulas Light",
-    "suite": "Apt. 556",
-    "city": "Gwenborough",
-    "zipcode": "92998-3874",
-    "geo": {
-      "lat": "-37.3159",
-      "lng": "81.1496"
-    }
-  },
-  "phone": "1-770-736-8031 x56442",
-  "website": "hildegard.org",
-  "company": {
-    "name": "Romaguera-Crona",
-    "catchPhrase": "Multi-layered client-server neural-net",
-    "bs": "harness real-time e-markets"
-  }
-},)
+// xhrData('POST','https://jsonplaceholder.typicode.com/users',{
+//   "id": 1,
+//   "name": "Leanne Graham",
+//   "username": "Bret",
+//   "email": "Sincere@april.biz",
+//   "address": {
+//     "street": "Kulas Light",
+//     "suite": "Apt. 556",
+//     "city": "Gwenborough",
+//     "zipcode": "92998-3874",
+//     "geo": {
+//       "lat": "-37.3159",
+//       "lng": "81.1496"
+//     }
+//   },
+//   "phone": "1-770-736-8031 x56442",
+//   "website": "hildegard.org",
+//   "company": {
+//     "name": "Romaguera-Crona",
+//     "catchPhrase": "Multi-layered client-server neural-net",
+//     "bs": "harness real-time e-markets"
+//   }
+// },)
