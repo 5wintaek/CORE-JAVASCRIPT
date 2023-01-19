21965e4c5743056ef3cb4084823ac15c79d1703a
5wintaek <elqkddl@naver.com>
2023-01-19 14:29:09 +0900

    npx 설치 후 서버 연결하기

--- client/main.js
+++ client/main.js
@@ -9,7 +9,8 @@ import {
   renderUserCard,
   renderSpinner,
   renderEmptyCard,
-  getNode
+  getNode,
+  attr
   
  } from "./lib/index.js";
 
@@ -34,7 +35,7 @@ async function rendingUserList(){
 
     $('.loadingSpinner').remove()
 
-    let response = await tiger.get( 'https://jsonplaceholder.typicode.com/users' );
+    let response = await tiger.get( ' http://localhost:3000/users' );
 
     let userData = response.data;
     // userData.forEach(data=> renderUserCard(userCardContainer,data))
@@ -62,12 +63,29 @@ rendingUserList();
 
 
 // 삭제 버튼을 클릭하면 콘솔창에 '삭제' 글자가 출력이 될 수 있도록 만들어 주세요
+// 버블링을 이용한 방법 ! 부모한테 위임을 시켜서 e.target 을 줌
+// closest('button')); -> 누른 버튼중에 가장 인접한 버튼만 찾아라
 
-getNode('.delte')
 
-userCardContainer.addEventListener('click',(e)=>{
-  console.log(e.target);
-})
+
+function handler(e){
+  let deleteButton = e.target.closest('button')
+  let article = e.target.closest('article')
+
+  
+  if(!deleteButton || !article) return;
+
+  let id = attr(article,'data-index').slice(5)
+
+  tiger.delete(` http://localhost:3000/users/${id}`)
+  
+  
+}
+
+userCardContainer.addEventListener('click',handler)
+
+
+
 
 
 
